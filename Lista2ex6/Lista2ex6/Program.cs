c57b3812e4788ba6a38e198cb23802bcbcbf09d0
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-12 19:22:41 -0300

    Add files via upload

---
+++ Lista2ex6/Lista2ex6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, altura, imc;
+
+            Console.Write("Digite seu peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            if (imc < 20 )
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            if (imc >= 20 && imc < 25)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            if (imc >= 25 )
+            {
+                Console.WriteLine("Acima do peso");
+            }
+        }
+    }
+}
