20471240b5a9f622459953bff40f7cb89629ee2c
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-12 19:22:20 -0300

    Add files via upload

---
+++ Lista2ex5/Lista2ex5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baze, altura, area;
+
+            Console.WriteLine("Digite a base do retângulo: ");
+            baze = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do retângulo: ");
+            altura = double.Parse(Console.ReadLine());
+
+            area = baze * altura;
+
+            Console.WriteLine("A área do retângulo é:{0} ", area);
+
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+    }
+}
