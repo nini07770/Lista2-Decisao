1b5be039b5a1092a83f320f8c95df4e032c6c781
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-12 17:30:03 -0300

    Add files via upload

---
+++ Lista2ex2/Lista2ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2;
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("Maior valor: " + n1);
+            }
+            else if (n2 > n1)
+            {
+                Console.WriteLine("Maior valor: " + n2);
+            }
+
+            else
+            {
+                Console.WriteLine("Os valores são iguais, MULA.");
+            }
+        }
+    }
+}
